05dccbac766b71255255ee2f322e6b368f2c02ca
Hidayat Sagita <hidayat.sagita@gmail.com>
2016-07-01 14:45:41 +0700

    init

---
+++ README.md
@@ -0,0 +1,67 @@
+# phpartisanbot
+
+A Node.js-based Slack bot for running Laravel's `php artisan` through chat.
+
+This bot require [Slack Bot Token](https://my.slack.com/services/new/bot).
+
+
+
+## Available Command
+
+There are few commands available, following command sorted by usage steps :
+
+1. `register project <full_path> <project-name>` 
+
+   Where **full_path** is your laravel installation full path and **project-name** is your project name.
+
+   This command will register your project into this bot.
+
+2. `list projects` 
+
+   This command will list all of your registered projects
+
+3. `use <project-name>`
+
+   Where **project-name** is your registered project name. This command will make your project as main project and will be used globally when you run any Laravel's artisan command
+
+4. `run <artisan command>`
+
+   Where **artisan command** is anything from Laravel's artisan commands, you can see available commands [here](https://laravel.com/docs/5.1/artisan) or just type `run list`
+
+
+
+
+## Installation
+
+This should just works as any other Node.js application. 
+
+1. Pull this repo
+2. Go to **phpartisanbot** folder and run `npm install`
+3. Run `node bot.js` or you can use any Node.js manager like [PM2](https://github.com/Unitech/PM2/) : `pm2 start bot.js`
+
+
+
+
+## Example
+
+Register project :
+
+`register project /Application/MAMP/htdocs/laravel-project laravel`
+
+Use **laravel** project as main project
+
+`use laravel`
+
+Some artisan commands usage :
+
+1. `run route:list` this is similar to `php artisan route:list`
+2. `run make:controller FooController` similar to : `php artisan make:controller FooController`
+3. `run make:migration create_table_foo` similar to : `php artisan make:migration create_table_foo`
+
+And many more!
+
+
+
+## Screenshot
+
+![Screenshot](https://raw.githubusercontent.com/hdytsgt/phpartisanbot/master/screenshot.png)
---
+++ bot.js
@@ -0,0 +1,180 @@
+'use strict';
+
+/**
+ * API Key & Token
+ */
+const SlackToken   = 'xoxb-56031931990-klSHUtBWn9c7l68sKVd8xqGD';
+
+/**
+ * Define Required vars
+ */
+var botkit  = require( 'botkit' ),
+    exec    = require( 'child_process' ).exec;
+
+/**
+ * Build up Slack
+ */
+var controller = botkit.slackbot({
+                    json_file_store: 'storage'
+                 });
+var bot        = controller.spawn({ 
+                    token: SlackToken
+                 });
+
+/**
+ * Fireup Slack's Real Time Messaging
+ */
+bot.startRTM( function( err, bot, payload ) {
+	if( err ){
+		throw new Error( 'Could not connect to Slack' );
+	}
+});
+
+/**
+ * Bot Listening...
+ */
+controller
+	.hears( [ 'register project (.*) (.*)' ], 'direct_message,direct_mention,mention', function( bot, message ) {
+	    BOT.registerProject( bot, message, message.match[ 1 ], message.match[ 2 ] );
+	})
+	.hears( [ 'list projects' ], 'direct_message,direct_mention,mention', function( bot, message ) {
+	    BOT.listProject( bot, message, true );
+	})
+	.hears( [ 'use (.*)' ], 'direct_message,direct_mention,mention', function( bot, message ) {
+	    BOT.setProject( bot, message, message.match[1] );
+	})
+	.hears( [ 'run (.*)' ], 'direct_message,direct_mention,mention', function( bot, message ) {
+	    BOT.runCommand( bot, message, message.match[1] );
+	});
+
+/**
+ * BOT Class
+ */
+var BOT = {
+
+	runCommand: function( bot, message, command ) {
+		
+		controller.storage.teams.get( 'current', function( error, data ) {
+
+			if( !error ) {
+
+				var projectName = data.project;
+
+				controller.storage.teams.get( projectName, function( error, data ) {
+
+					var _command = 'php ' + data.path + '/artisan ' + command
+
+					bot.reply( message, 'Executing your command under *'+ projectName +'*.... :hourglass:' );
+
+					exec( _command, function( error, stdout, stderr ) {
+
+						if( error )
+							bot.reply( message, error );
+
+						if( stdout )
+							bot.reply( message, '```' + stdout + '```' );
+						
+						if( stderr )
+							bot.reply( message, '```stderr : ' + stderr + '```' );
+
+					});
+
+				});
+			} else {
+				bot.reply( message, 'You haven\'t set any project sir! :confused: Try use `use <project-name>`' );
+			}
+
+		});
+	},
+
+	registerProject: function( bot, message, path, projectName ) {
+
+		var _invalid = 'You already have a project with this name sir! :pensive: Check your project by `list projects`';
+
+		BOT.validateProject( bot, message, projectName, _invalid, function() {
+
+			controller.storage.teams.save({
+				id : projectName,
+				path : path
+			}, function( error ) {
+
+				if( error ) throw new Error( 'Something wrong while registering project : ' + error );
+
+				bot.reply( message, 'Registering your project sir! Hold on a sec... :hourglass:' );
+				bot.reply( message, 'Okay! Project is registered sir! :tada: Here are your project list :point_down:' );
+				BOT.listProject( bot, message );
+			
+			});
+		});
+	},
+
+	listProject: function( bot, message, displayMessage = false ) {
+
+		var _projects = '';
+
+		controller.storage.teams.all( function( error, data ) {
+
+			if( error ) throw new Error( 'Something wrong while registering project : ' + error );
+
+			if( typeof data !== 'undefined' && data.length ) {
+
+				if( displayMessage )
+					bot.reply( message, 'Here are your registered projects so far :point_down:' );
+
+				var _count = 0;
+
+				data.map( function( project ) {
+					if( project.id != 'current' )
+						_projects = _projects + ( ++_count ) + '. *' + project.id + '*\n';
+				});
+
+				bot.reply( message, _projects );
+
+			} else {
+
+				if( displayMessage )
+					bot.reply( message, 'You don\'t have project registered :confused: Try adding one : `register project <full/path> <project-name>`' );
+			
+			}
+		});
+	},
+
+	validateProject: function( bot, message, projectName, invalidMessage, callback ) {
+
+		controller.storage.teams.get( projectName, function( error, data ) {
+
+			if( !error ) {
+				bot.reply( message, invalidMessage );
+			} else {
+				callback();
+			}
+			
+		});
+	},
+
+	setProject: function( bot, message, projectName ) {
+
+		controller.storage.teams.get( projectName, function( error, data ) {
+
+			if( !error ) {
+
+				controller.storage.teams.save({
+					id : 'current',
+					project : projectName
+				}, function( error ) {
+
+					if( error ) throw new Error( 'Something wrong while registering project : ' + error );
+
+					bot.reply( message, 'Okay! *'+ projectName +'* is activated sir! :thumbsup:' );
+				
+				});
+
+			} else {
+
+				bot.reply( message, 'Project *'+ projectName +'* is not exists sir! :confused:' );
+			
+			}
+		});
+	}
+
+};
